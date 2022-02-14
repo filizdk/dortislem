e111a356257d14c5e40cbfb620f4a7cf77acc9fa
Ozgur Demir <ozgur.demir@itutor.com>
2022-02-13 21:29:16 -0800

    Topla yerine geri kondu

--- dortislem.go
+++ dortislem.go
@@ -1,9 +1,13 @@
 package dortislem
 
-// func Topla(x int, y int) int {
-// 	return x + y
-// }
+func Topla(x int, y int) int {
+	return x + y
+}
 
 func Carp(x int, y int) int {
 	return x * y
+}
+
+func Cikar(x int, y int) int {
+	return x - y
 }
